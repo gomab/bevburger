d68762a2ba14ae47959c06f1cb6f4cf7192ba5eb
gomab <egoiomab@gamil.com>
2018-04-09 03:11:34 +0200

    Item Model & Relationships with Category

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['name', 'slug'];
+
+    /*
+     * Une Categorie a plusieurs Item
+     */
+    public function items(){
+        return $this->hasMany('App\Item');
+    }
 }
